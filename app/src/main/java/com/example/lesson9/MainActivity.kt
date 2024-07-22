1cda69f81cc1fddfbc84ef0d91df331026decddd
dariakolpakova <daka.kolp01@gmail.com>
2024-07-22 15:21:04 +0300

    main_layout added

--- app/src/main/java/com/example/lesson9/MainActivity.kt
+++ app/src/main/java/com/example/lesson9/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.lesson9
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
 
-class MainActivity : ComponentActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        setContentView(R.layout.main_layout)
     }
 }
